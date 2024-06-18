b66e7ffff488e07fa857d9a1d286f4671b4e4e92
Gug4ZL <gustavo2004ct@gmail.com>
2024-06-18 17:29:45 -0300

    add carrosel de fotos da sobrancelha

--- scripts.js
+++ scripts.js
@@ -1,7 +1,13 @@
 let prevButton = document.getElementById('prev');
 let nextButton = document.getElementById('next');
 let eyeBrow = document.querySelector('.container-eyebrow');
-let items = eyeBrow.querySelectorAll('.eyebrow .item')
+let items = eyeBrow.querySelectorAll('.eyebrow .item');
+let linksWindow = document.querySelector('.links')
+let bntClose = document.getElementById('btn-close')
+
+let slideEyeBrow = 100;
+let openEyeBrow = 0;
+
 
 let active = 0;
 let firstPosition = 0;
@@ -11,6 +17,7 @@ let lastPosition = items.length - 1;
 if (!eyeBrow.querySelector('.eyebrow .item.active')) {
     items[0].classList.add('active');
 }
+
 
 nextButton.onclick = () => {
     let itemOld = eyeBrow.querySelector('.eyebrow .item.active');
@@ -26,8 +33,6 @@ nextButton.onclick = () => {
 };
 
 
-
-
 prevButton.onclick = () => {
     let itemOld = eyeBrow.querySelector('.eyebrow .item.active');
     if (itemOld) {
@@ -40,3 +45,23 @@ prevButton.onclick = () => {
         itemNew.classList.add('active');
     }
 };
+
+
+linksWindow.onclick = () => {
+    eyeBrow.style.opacity = 1
+    eyeBrow.style.transform = "translateX("+ openEyeBrow + "vw)"
+    eyeBrow.style.position = "fixed"
+    eyeBrow.style.top = "50%"
+    eyeBrow.style.left = "50%"
+}
+
+bntClose.onclick = () => {
+   eyeBrow.style.opacity = 0
+   eyeBrow.style.position = "fixed"
+   eyeBrow.style.left = "170%"
+
+
+
+//    eyeBrow.style.transform = "translateX("+ slideEyeBrow + "vw)"
+}
+
