b13019f1b167ca93d8713ecc80fa60b1bd7bda64
Gug4ZL <gustavo2004ct@gmail.com>
2024-06-19 16:37:28 -0300

    add carrosel de fotos das unhas

--- scripts.js
+++ scripts.js
@@ -5,6 +5,13 @@ let items = eyeBrow.querySelectorAll('.eyebrow .item');
 let linksWindow = document.querySelector('.links')
 let bntClose = document.getElementById('btn-close')
 
+let btnPrev = document.getElementById('btn-prev')
+let btnNext = document.getElementById('btn-next')
+let nails = document.querySelector('.container-nails')
+let itens = nails.querySelectorAll('.nails .item')
+let ButtonClose = document.getElementById('close')
+let nailLink = document.querySelector('.link-nail')
+
 let slideEyeBrow = 100;
 let openEyeBrow = 0;
 
@@ -12,6 +19,7 @@ let openEyeBrow = 0;
 let active = 0;
 let firstPosition = 0;
 let lastPosition = items.length - 1;
+let nailsPosition = itens.length - 1;
 
 
 if (!eyeBrow.querySelector('.eyebrow .item.active')) {
@@ -46,22 +54,64 @@ prevButton.onclick = () => {
     }
 };
 
-
-linksWindow.onclick = () => {
-    eyeBrow.style.opacity = 1
-    eyeBrow.style.transform = "translateX("+ openEyeBrow + "vw)"
-    eyeBrow.style.position = "fixed"
-    eyeBrow.style.top = "50%"
-    eyeBrow.style.left = "50%"
+if (!nails.querySelector('.nails .item.active')) {
+    itens[0].classList.add('active');
 }
 
-bntClose.onclick = () => {
-   eyeBrow.style.opacity = 0
-   eyeBrow.style.position = "fixed"
-   eyeBrow.style.left = "170%"
 
+btnNext.onclick = () => {
+    let itemOld = nails.querySelector('.nails .item.active');
+    if (itemOld) {
+        itemOld.classList.remove('active');
+    }
 
-
-//    eyeBrow.style.transform = "translateX("+ slideEyeBrow + "vw)"
+    active = (active + 1 > nailsPosition) ? firstPosition : active + 1;
+    let itemNew = itens[active];
+    if (itemNew) {
+        itemNew.classList.add('active');
+    }
 }
+
+
+btnPrev.onclick = () => {
+    let itemOld = nails.querySelector('.nails .item.active');
+    if (itemOld) {
+        itemOld.classList.remove('active');
+    }
+
+    active = (active - 1 < firstPosition) ? nailsPosition : active - 1;
+    let itemNew = itens[active];
+    if (itemNew) {
+        itemNew.classList.add('active');
+    }
+}
+    
+
+    linksWindow.onclick = () => {
+        eyeBrow.style.opacity = 1
+        eyeBrow.style.transform = "translateX(" + openEyeBrow + "vw)"
+        eyeBrow.style.position = "fixed"
+        eyeBrow.style.top = "50%"
+        eyeBrow.style.left = "50%"
+    }
+
+    bntClose.onclick = () => {
+        eyeBrow.style.opacity = 0
+        eyeBrow.style.position = "fixed"
+        eyeBrow.style.left = "170%"
+    }
+
+    ButtonClose.onclick = () => {
+        nails.style.opacity = 0
+        nails.style.position = "fixed"
+        nails.style.left = "170%"
+    }
+
+    nailLink.onclick = () =>{
+        nails.style.opacity = 1
+        nails.style.transform = "translateX(" + openEyeBrow + "vw)"
+        nails.style.position = "fixed"
+        nails.style.top = "50%"
+        nails.style.left = "50%"
+    }
 
